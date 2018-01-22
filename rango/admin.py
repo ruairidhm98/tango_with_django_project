84a776100a55a9a7c12d512ddeaf74d15d732ffe
Ruairidh MacGregor <ruairidhmacgregor@ruairidhs-mbp.home>
2018-01-22 21:28:13 +0000

    Third commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+    def __str__(self):
+        return self.list_display
+
+
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
