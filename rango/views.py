4b0b814240b361d1e563bc68b6e0ad06cf41e50c
Ruairidh MacGregor <ruairidhmacgregor@ruairidhs-mbp.home>
2018-01-28 14:01:02 +0000

    Chapter 7 (exercises not complete)

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
 
 def show_category(request, category_name_slug):
     context_dict = {}
