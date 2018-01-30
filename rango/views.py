9f81d6becd644b98576ca8bef4af94bf70cd96ad
Ruairidh MacGregor <ruairidhmacgregor@ruairidhs-mbp.home>
2018-01-30 23:23:22 +0000

    Chapter 10

--- rango/views.py
+++ rango/views.py
@@ -9,6 +9,31 @@ from rango.models import Page
 from rango.forms import CategoryForm
 from rango.forms import PageForm
 from rango.forms import UserForm, UserProfileForm
+from datetime import datetime
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(request.COOKIES.get('visits', '1'))
+
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                       '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits += 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
 
 
 @login_required
@@ -129,14 +154,29 @@ def show_category(request, category_name_slug):
 
 
 def index(request):
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by("-likes")[:5]
     page_list = Page.objects.order_by("-views")[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
 
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'rango/index.html', context_dict)
+    return response
 
 
 def about(request):
-    context_dict = {'boldmessage': "This tutorial has been put together by Ruairidh Macgregor"}
+    # link to template and provide dictionary for Django variables within templates
+    context_dict = {
+        'aboutmessage': "This tutorial has been put together by Ruairidh Macgregor."
+    }
 
-    return render(request, 'rango/about.html', context=context_dict)
+    # Call helper function to handle cookies
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    # Get response early so we can gather cookie info
+    response = render(request, 'rango/about.html', context_dict)
+    # Get response for client and return it (updating cookies if need be)
+    return response
